2535af1f5d41b4719714c0a269f000ab8e450ccb
hhchan94 <hhchan1@uw.edu>
2024-11-27 19:26:27 -0800

    add testing code for parser.py

--- src/tests/test_parser.py
+++ src/tests/test_parser.py
@@ -10,9 +10,16 @@ Tests the functions in parser.py
 # TODO: Create expected output from .txt files.
 # Outputs should be lists of strings, where each line is a separate string
 #   and all words are normalized (i.e., lowercase without punctuation)
-expected_single_mixed = ["Arma virumque cano, Troiae qui primus ab oris"]
-expected_multiline = ['Arma virumque cano, Troiae qui primus ab oris', 'Italiam, fato profugus, Laviniaque venit', 'litora, multum ille et terris iactatus et alto', 'vi superum saevae memorem Iunonis ob iram;', 'multa quoque et bello passus, dum conderet urbem,\n', 'inferretque deos Latio, genus unde Latinum,', 'Albanique patres, atque altae moenia Romae.']
-
+expected_single_mixed = ["arma virumque cano troiae qui primus ab oris"]
+expected_multiline = ['arma virumque cano troiae qui primus ab oris',
+ 'italiamm fato profugus laviniaque venit litora',
+ 'multum ille et terris iactatus et alto vi superum saevae memorem iunonis ob iram',
+ 'multa quoque et bello passus dum conderet urbem',
+ 'inferretque deos latio genus unde latinum',
+ 'albanique patres atque altae moenia romae']
+expected_mixed_elision = ["litora multum ille et terris iactatus et alto"]
+expected_single_m_elision = ["venturum excidio libyae sic volvere parcas"]
+expected_single_vowel_elision = ["turbine corripuit scopuloque infixit acuto"]
 # pseudo-test:
 # test_parse_txt <- result of this should be == expected
 # test_parse_xml <- result of this should be == expected
@@ -32,13 +39,26 @@ def test_parse_txt():
 
 def test_parse_xml():
     # mixed_elision
+    path_file = "CARMINA/examples/aeneid_mixed_elision.xml"
+    result = parse_xml(path_file) 
+    assert result == expected_mixed_elision
 
     # multiline
+    path_file = "CARMINA/examples/aeneid_multiline.xml"
+    result = parse_xml(path_file) 
+    assert result == expected_multiline
 
     # single_m_elision
+    path_file = "CARMINA/examples/aeneid_single_m_elision.xml"
+    result = parse_xml(path_file) 
+    assert result == expected_single_m_elision
 
     # single_mixed
+    path_file = "CARMINA/examples/aeneid_single_mixed.xml"
+    result = parse_xml(path_file) 
+    assert result == expected_single_mixed
 
     # single_vowel_elision
-    
-    pass
+    path_file = "CARMINA/examples/aeneid_single_vowel_elision.xml"
+    result = parse_xml(path_file) 
+    assert result == expected_single_vowel_elision
